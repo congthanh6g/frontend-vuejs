0e1c9ad9943932ceb41803c57eaef79e1c227689
congthanh6g <65008703+congthanh6g@users.noreply.github.com>
2022-03-27 13:44:55 +0700

    commit

---
+++ src/services/auth.service.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+const API_URL = 'https://heroku-springbackend.herokuapp.com/api/auth/';
+
+class AuthService {
+    login(user) {
+        return axios
+                    .post(API_URL + 'signin' , {
+                        username : user.username ,
+                        password : user.password
+                    })
+                    .then(response => {
+                        if(response.data.token) {
+                            localStorage.setItem('user' , JSON.stringify(response.data));
+                        }
+                        return response.data;
+                    });
+    }
+    logout() {
+        localStorage.removeItem('user');
+    }
+}
+export default new AuthService();
